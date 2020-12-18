c0f19ff7c1dd30b601d456227cfb15fa4d07d03a
Chris Gray <christophermjgray@gmail.com>
2020-12-18 13:31:59 -0500

    Update initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -3,8 +3,8 @@
 #-------------------------------------------------------------
 #  * Copyright (c) 2021 Christopher Gray
 #  * All rights reserved.  Proprietary and Confidential.
-# Version: 0.0.15
-# Updated: 11/21/2020
+# Version: 0.0.16
+# Updated: 12/18/2020
 # ChangeLog:
 #
 # Sources:
@@ -66,6 +66,8 @@ cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_
 cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (4, 'Blocks_Video', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Blocks: Streaming Video. ie: Youtube/Google Video')")
 cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (5, 'Blocks_Social', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Blocks: Social Media. ie: Facebook/Instagram/Twitter')")
 cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (6, 'Allow_Adult', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Allow custom domains')")
+cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (7, 'Allow_Social', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Allow Social Media domains')")
+cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (8, 'Allow_Chat', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Allow Chat domains')")
 
 
 #cur.execute("INSERT OR IGNORE INTO 'group' (id, name, enabled, date_added, date_modified, description) VALUES (4, 'Children_Good', 1, " + UnixEpoch + "," + UnixEpoch + ", 'Blocks malware,ads and porn')")
